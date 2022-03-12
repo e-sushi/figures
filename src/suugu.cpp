658bdf6cacea04294d875ff6fbe6593d3a6ce4e7
e-sushi <officialaxos7@gmail.com>
2022-03-12 16:49:59 -0500

    remove outdated comment

--- src/suugu.cpp
+++ src/suugu.cpp
@@ -1,35 +1,4 @@
 ﻿/* suugu
-
-current brakus naur idea
-this is basically the grammar we expect on input
-and also helps determine precedence
-which we will turn into our own internal format
-only basic arithmetic like +-/* and bitwise stuff for now
-
-<input>         :: = <exp>
-<exp>           :: = <bitwise or>
-<bitwise or>    :: = <bitwise xor> { "|" <bitwise xor> }
-<bitwise xor>   :: = <bitwise and> { "^" <bitwise and> }
-<bitwise and>   :: = <equality> { "&" <equality> }
-<equality>      :: = <relational> { ("!=" | "==") <relational> }
-<relational>    :: = <bitwise shift> { ("<" | ">" | "<=" | ">=") <bitwise shift> }
-<bitwise shift> :: = <additive> { ("<<" | ">>" ) <additive> }
-<additive>      :: = <term> { ("+" | "-") <term> }
-<term>          :: = <factor> { ("*" | "/" | "%") <factor> }
-<factor>        :: = "(" <exp> ")" | <unary> <factor> | <literal>
-<unary>         :: = "!" | "~" | "-"
-
-ideas for parsing itself:
-- we only parse when a key is input 
-- we only indicate errors when we cant make a tree from what we've parsed
-  for ex.
-  we do not indicate an error on "3 + " because we are expecting the user to input something
-  we do indicate an error on "3 + sin()" because the user missed inputting a literal into the fucntion
-  or instead of erroring, we put an empty box where we expect something thats missing
-  and only error in cases where the user uses invalid syntax such as "3 <<- 5"
-  or if they use an undefined identifier, etc.
-
-
 TODO Board
 ----------------------------------------------------
 most math should be f64 instead of f32
