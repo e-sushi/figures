47645f6e86cd73d9680f3bfbe4a541d3b10b940b
dellevelled <delle.caj@gmail.com>
2022-04-24 05:34:09 -0400

    Sync input and time interface changes from deshi

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -46,51 +46,45 @@ local CanvasTool previous_tool = CanvasTool_Navigation;
 ////////////////
 enum CanvasBind_{ //TODO ideally support multiple keybinds per action
 	//[GLOBAL] SetTool
-	CanvasBind_SetTool_Navigation = Key::ESCAPE  | InputMod_Any,
-	CanvasBind_SetTool_Context    = Key::MBRIGHT | InputMod_AnyCtrl,
-	CanvasBind_SetTool_Expression = Key::E       | InputMod_AnyCtrl, //NOTE temp making this CTRL+E for simplicity
-	CanvasBind_SetTool_Pencil     = Key::P       | InputMod_AnyCtrl,
-	CanvasBind_SetTool_Graph      = Key::G       | InputMod_AnyCtrl,
-	CanvasBind_SetTool_Previous   = Key::MBFOUR  | InputMod_None,
+	CanvasBind_SetTool_Navigation = Key_ESCAPE  | InputMod_Any,
+	CanvasBind_SetTool_Context    = Mouse_RIGHT | InputMod_AnyCtrl,
+	CanvasBind_SetTool_Expression = Key_E       | InputMod_AnyCtrl, //NOTE temp making this CTRL+E for simplicity
+	CanvasBind_SetTool_Pencil     = Key_P       | InputMod_AnyCtrl,
+	CanvasBind_SetTool_Graph      = Key_G       | InputMod_AnyCtrl,
+	CanvasBind_SetTool_Previous   = Mouse_4     | InputMod_None,
 	
 	//[GLOBAL] Camera 
-	CanvasBind_Camera_Pan     = Key::MBMIDDLE     | InputMod_None,
-	CanvasBind_Camera_ZoomIn  = Key::MBSCROLLUP   | InputMod_None,
-	CanvasBind_Camera_ZoomOut = Key::MBSCROLLDOWN | InputMod_None,
+	CanvasBind_Camera_Pan     = Mouse_MIDDLE | InputMod_None,
 	
 	//[LOCAL]  Navigation 
-	CanvasBind_Navigation_Pan       = Key::MBLEFT  | InputMod_Any,
-	CanvasBind_Navigation_ResetPos  = Key::NUMPAD0 | InputMod_None,
-	CanvasBind_Navigation_ResetZoom = Key::NUMPAD0 | InputMod_None,
+	CanvasBind_Navigation_Pan       = Mouse_LEFT  | InputMod_Any,
+	CanvasBind_Navigation_ResetPos  = Key_NP0     | InputMod_None,
+	CanvasBind_Navigation_ResetZoom = Key_NP0     | InputMod_None,
 	
 	//[LOCAL]  Expression
-	CanvasBind_Expression_Select                = Key::MBLEFT    | InputMod_None,
-	CanvasBind_Expression_Create                = Key::MBRIGHT   | InputMod_None,
-	CanvasBind_Expression_CursorLeft            = Key::LEFT      | InputMod_None,
-	CanvasBind_Expression_CursorRight           = Key::RIGHT     | InputMod_None,
-	CanvasBind_Expression_CursorWordLeft        = Key::LEFT      | InputMod_AnyCtrl,
-	CanvasBind_Expression_CursorWordRight       = Key::RIGHT     | InputMod_AnyCtrl,
-	CanvasBind_Expression_CursorUp              = Key::UP        | InputMod_None,
-	CanvasBind_Expression_CursorDown            = Key::DOWN      | InputMod_None,
-	CanvasBind_Expression_CursorHome            = Key::HOME      | InputMod_None,
-	CanvasBind_Expression_CursorEnd             = Key::END       | InputMod_None,
-	CanvasBind_Expression_CursorDeleteLeft      = Key::BACKSPACE | InputMod_None,
-	CanvasBind_Expression_CursorDeleteRight     = Key::DELETE    | InputMod_None,
-	CanvasBind_Expression_CursorDeleteWordLeft  = Key::BACKSPACE | InputMod_AnyCtrl,
-	CanvasBind_Expression_CursorDeleteWordRight = Key::DELETE    | InputMod_AnyCtrl,
+	CanvasBind_Expression_Select                = Mouse_LEFT    | InputMod_None,
+	CanvasBind_Expression_Create                = Mouse_RIGHT   | InputMod_None,
+	CanvasBind_Expression_CursorLeft            = Key_LEFT      | InputMod_None,
+	CanvasBind_Expression_CursorRight           = Key_RIGHT     | InputMod_None,
+	CanvasBind_Expression_CursorWordLeft        = Key_LEFT      | InputMod_AnyCtrl,
+	CanvasBind_Expression_CursorWordRight       = Key_RIGHT     | InputMod_AnyCtrl,
+	CanvasBind_Expression_CursorUp              = Key_UP        | InputMod_None,
+	CanvasBind_Expression_CursorDown            = Key_DOWN      | InputMod_None,
+	CanvasBind_Expression_CursorHome            = Key_HOME      | InputMod_None,
+	CanvasBind_Expression_CursorEnd             = Key_END       | InputMod_None,
+	CanvasBind_Expression_CursorDeleteLeft      = Key_BACKSPACE | InputMod_None,
+	CanvasBind_Expression_CursorDeleteRight     = Key_DELETE    | InputMod_None,
+	CanvasBind_Expression_CursorDeleteWordLeft  = Key_BACKSPACE | InputMod_AnyCtrl,
+	CanvasBind_Expression_CursorDeleteWordRight = Key_DELETE    | InputMod_AnyCtrl,
 	
 	//[LOCAL]  Pencil
-	CanvasBind_Pencil_Stroke             = Key::MBLEFT       | InputMod_Any,
-	CanvasBind_Pencil_SizeIncrementBy1   = Key::MBSCROLLUP   | InputMod_AnyShift,
-	CanvasBind_Pencil_SizeIncrementBy5   = Key::MBSCROLLUP   | InputMod_AnyCtrl,
-	CanvasBind_Pencil_SizeDecrementBy1   = Key::MBSCROLLDOWN | InputMod_AnyShift,
-	CanvasBind_Pencil_SizeDecrementBy5   = Key::MBSCROLLDOWN | InputMod_AnyCtrl,
-	CanvasBind_Pencil_DeletePrevious     = Key::Z            | InputMod_AnyCtrl,
-	CanvasBind_Pencil_DetailIncrementBy1 = Key::EQUALS       | InputMod_None,
-	CanvasBind_Pencil_DetailIncrementBy5 = Key::EQUALS       | InputMod_AnyShift,
-	CanvasBind_Pencil_DetailDecrementBy1 = Key::MINUS        | InputMod_None,
-	CanvasBind_Pencil_DetailDecrementBy5 = Key::MINUS        | InputMod_AnyShift,
-}; typedef Key::Key CanvasBind;
+	CanvasBind_Pencil_Stroke             = Mouse_LEFT       | InputMod_Any,
+	CanvasBind_Pencil_DeletePrevious     = Key_Z            | InputMod_AnyCtrl,
+	CanvasBind_Pencil_DetailIncrementBy1 = Key_EQUALS       | InputMod_None,
+	CanvasBind_Pencil_DetailIncrementBy5 = Key_EQUALS       | InputMod_AnyShift,
+	CanvasBind_Pencil_DetailDecrementBy1 = Key_MINUS        | InputMod_None,
+	CanvasBind_Pencil_DetailDecrementBy5 = Key_MINUS        | InputMod_AnyShift,
+}; typedef KeyCode CanvasBind;
 
 
 /////////////////
@@ -491,40 +485,40 @@ void update_canvas(){
 	
 	//-///////////////////////////////////////////////////////////////////////////////////////////////
 	//// @input
-	mouse_pos_world = ToWorld(DeshInput->mousePos);
+	mouse_pos_world = ToWorld(input_mouse_position());
 	
 	//// @input_tool ////
-	if      (DeshInput->KeyPressed(CanvasBind_SetTool_Navigation) && active_tool != CanvasTool_Navigation){
+	if      (key_pressed(CanvasBind_SetTool_Navigation) && active_tool != CanvasTool_Navigation){
 		previous_tool = active_tool;
 		active_tool   = CanvasTool_Navigation;
-	}else if(DeshInput->KeyPressed(CanvasBind_SetTool_Context)    && active_tool != CanvasTool_Context){
+	}else if(key_pressed(CanvasBind_SetTool_Context)    && active_tool != CanvasTool_Context){
 		previous_tool = active_tool;
 		active_tool   = CanvasTool_Context;
-	}else if(DeshInput->KeyPressed(CanvasBind_SetTool_Expression) && active_tool != CanvasTool_Expression){
+	}else if(key_pressed(CanvasBind_SetTool_Expression) && active_tool != CanvasTool_Expression){
 		previous_tool = active_tool;
 		active_tool   = CanvasTool_Expression;
-	}else if(DeshInput->KeyPressed(CanvasBind_SetTool_Pencil)     && active_tool != CanvasTool_Pencil){
+	}else if(key_pressed(CanvasBind_SetTool_Pencil)     && active_tool != CanvasTool_Pencil){
 		previous_tool = active_tool;
 		active_tool   = CanvasTool_Pencil;
-	}else if(DeshInput->KeyPressed(CanvasBind_SetTool_Graph)){
+	}else if(key_pressed(CanvasBind_SetTool_Graph)){
 		activeGraph   = (activeGraph) ? 0 : &defgraph;
-	}else if(DeshInput->KeyPressed(CanvasBind_SetTool_Previous)){
+	}else if(key_pressed(CanvasBind_SetTool_Previous)){
 		Swap(previous_tool, active_tool);
 	}
 	
 	//// @input_camera ////
-	if(DeshInput->KeyPressed(CanvasBind_Camera_Pan)){
+	if(key_pressed(CanvasBind_Camera_Pan)){
 		camera_pan_active = true;
-		camera_pan_mouse_pos = DeshInput->mousePos;
+		camera_pan_mouse_pos = input_mouse_position();
 		camera_pan_start_pos = camera_pos;
 	}
-	if(DeshInput->KeyDown(CanvasBind_Camera_Pan)){
+	if(key_down(CanvasBind_Camera_Pan)){
 		camera_pos = camera_pan_start_pos + (ToWorld(camera_pan_mouse_pos) - mouse_pos_world);
 	}
-	if(DeshInput->KeyReleased(CanvasBind_Camera_Pan)){
+	if(key_released(CanvasBind_Camera_Pan)){
 		camera_pan_active = false;
 	}
-	if(DeshInput->scrollY != 0 && DeshInput->ModsDown(InputMod_None) && !UI::AnyWinHovered()){
+	if(DeshInput->scrollY != 0 && input_mods_down(InputMod_None) && !UI::AnyWinHovered()){
 		if(!activeGraph){
 			camera_zoom -= camera_zoom / 10.0 * DeshInput->scrollY;
 			camera_zoom = Clamp(camera_zoom, 1e-37, 1e37);
@@ -575,16 +569,16 @@ void update_canvas(){
 	switch(active_tool){
 		//// @input_navigation ////
 		case CanvasTool_Navigation: if(!UI::AnyWinHovered()){
-			if(DeshInput->KeyPressed(CanvasBind_Navigation_Pan)){
+			if(key_pressed(CanvasBind_Navigation_Pan)){
 				camera_pan_active = true;
-				camera_pan_mouse_pos = DeshInput->mousePos;
+				camera_pan_mouse_pos = input_mouse_position();
 				if(!activeGraph){
 					camera_pan_start_pos = camera_pos;
 				}else{
 					camera_pan_start_pos = vec2f64{activeGraph->graph->cameraPosition.x, activeGraph->graph->cameraPosition.y};
 				}
 			}
-			if(DeshInput->KeyDown(CanvasBind_Navigation_Pan)){
+			if(key_down(CanvasBind_Navigation_Pan)){
 				if(!activeGraph){
 					camera_pos = camera_pan_start_pos + (ToWorld(camera_pan_mouse_pos) - mouse_pos_world);
 				}else{
@@ -592,17 +586,17 @@ void update_canvas(){
 					activeGraph->graph->cameraPosition.y = (camera_pan_start_pos.y + (camera_pan_mouse_pos.y - DeshInput->mouseY) / activeGraph->graph->dimensions_per_unit_length.y);
 				}
 			}
-			if(DeshInput->KeyReleased(CanvasBind_Navigation_Pan)){
+			if(key_released(CanvasBind_Navigation_Pan)){
 				camera_pan_active = false;
 			}
-			if(DeshInput->KeyPressed(CanvasBind_Navigation_ResetPos)){
+			if(key_pressed(CanvasBind_Navigation_ResetPos)){
 				if(!activeGraph){
 					camera_pos = {0,0};
 				}else{
 					activeGraph->graph->cameraPosition = {0,0};
 				}
 			}
-			if(DeshInput->KeyPressed(CanvasBind_Navigation_ResetZoom)){
+			if(key_pressed(CanvasBind_Navigation_ResetZoom)){
 				if(!activeGraph){
 					camera_zoom = 1.0;
 				}else{
@@ -620,7 +614,7 @@ void update_canvas(){
 		
 		//// @input_expression ////
 		case CanvasTool_Expression: if(!UI::AnyWinHovered()){
-			if(DeshInput->KeyPressed(CanvasBind_Expression_Select)){
+			if(key_pressed(CanvasBind_Expression_Select)){
 				selected_element = 0;
 				//TODO inverse the space transformation here since mouse pos is screen space, which is less precise being
 				//  elevated to higher precision, instead of higher precision world space getting transformed to screen space
@@ -635,7 +629,7 @@ void update_canvas(){
 				}
 			}
 			
-			if(DeshInput->KeyPressed(CanvasBind_Expression_Create)){
+			if(key_pressed(CanvasBind_Expression_Create)){
 				Expression* expr = (Expression*)memory_alloc(sizeof(Expression)); //TODO expression arena
 				expr->element.x       = mouse_pos_world.x;
 				expr->element.y       = mouse_pos_world.y;
@@ -656,13 +650,13 @@ void update_canvas(){
 				b32 ast_changed = false;
 				
 				//// @input_expression_cursor ////
-				if(expr->cursor_start > 1 && DeshInput->KeyPressed(CanvasBind_Expression_CursorLeft)){
+				if(expr->cursor_start > 1 && key_pressed(CanvasBind_Expression_CursorLeft)){
 					expr->cursor_start -= 1;
 				}
-				if(expr->cursor_start < expr->raw.count && DeshInput->KeyPressed(CanvasBind_Expression_CursorRight)){
+				if(expr->cursor_start < expr->raw.count && key_pressed(CanvasBind_Expression_CursorRight)){
 					expr->cursor_start += 1;
 				}
-				if(expr->cursor_start > 1 && DeshInput->KeyPressed(CanvasBind_Expression_CursorWordLeft)){
+				if(expr->cursor_start > 1 && key_pressed(CanvasBind_Expression_CursorWordLeft)){
 					if(*(expr->raw.str+expr->cursor_start-1) == ')'){
 						while(expr->cursor_start > 1 && *(expr->raw.str+expr->cursor_start-1) != '('){
 							expr->cursor_start -= 1;
@@ -676,7 +670,7 @@ void update_canvas(){
 						}
 					}
 				}
-				if(expr->cursor_start < expr->raw.count && DeshInput->KeyPressed(CanvasBind_Expression_CursorWordRight)){
+				if(expr->cursor_start < expr->raw.count && key_pressed(CanvasBind_Expression_CursorWordRight)){
 					if(*(expr->raw.str+expr->cursor_start) == '('){
 						while(expr->cursor_start < expr->raw.count && *(expr->raw.str+expr->cursor_start) != ')'){
 							expr->cursor_start += 1;
@@ -690,10 +684,10 @@ void update_canvas(){
 						}
 					}
 				}
-				if(DeshInput->KeyPressed(CanvasBind_Expression_CursorHome)){
+				if(key_pressed(CanvasBind_Expression_CursorHome)){
 					expr->cursor_start = 1;
 				}
-				if(DeshInput->KeyPressed(CanvasBind_Expression_CursorEnd)){
+				if(key_pressed(CanvasBind_Expression_CursorEnd)){
 					expr->cursor_start = expr->raw.count;
 				}
 				
@@ -708,16 +702,16 @@ void update_canvas(){
 				}
 				
 				//// @input_expression_deletion ////
-				if(expr->cursor_start > 1 && DeshInput->KeyPressed(CanvasBind_Expression_CursorDeleteLeft)){
+				if(expr->cursor_start > 1 && key_pressed(CanvasBind_Expression_CursorDeleteLeft)){
 					ast_changed = true;
 					expr->raw.erase(expr->cursor_start-1);
 					expr->cursor_start -= 1;
 				}
-				if(expr->cursor_start < expr->raw.count-1 && DeshInput->KeyPressed(CanvasBind_Expression_CursorDeleteRight)){
+				if(expr->cursor_start < expr->raw.count-1 && key_pressed(CanvasBind_Expression_CursorDeleteRight)){
 					ast_changed = true;
 					expr->raw.erase(expr->cursor_start);
 				}
-				if(expr->cursor_start > 1 && DeshInput->KeyPressed(CanvasBind_Expression_CursorDeleteWordLeft)){
+				if(expr->cursor_start > 1 && key_pressed(CanvasBind_Expression_CursorDeleteWordLeft)){
 					ast_changed = true;
 					if(*(expr->raw.str+expr->cursor_start-1) == ')'){
 						while(expr->cursor_start > 1 && *(expr->raw.str+expr->cursor_start-1) != '('){
@@ -738,7 +732,7 @@ void update_canvas(){
 						}
 					}
 				}
-				if(expr->cursor_start < expr->raw.count-1 && DeshInput->KeyPressed(CanvasBind_Expression_CursorDeleteWordRight)){
+				if(expr->cursor_start < expr->raw.count-1 && key_pressed(CanvasBind_Expression_CursorDeleteWordRight)){
 					ast_changed = true;
 					if(*(expr->raw.str+expr->cursor_start) == '('){
 						while(expr->cursor_start < expr->raw.count && *(expr->raw.str+expr->cursor_start) != ')'){
@@ -765,34 +759,34 @@ void update_canvas(){
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		//// @input_pencil
 		case CanvasTool_Pencil: if(!UI::AnyWinHovered()){
-			if(DeshInput->KeyPressed(CanvasBind_Pencil_Stroke)){
+			if(key_pressed(CanvasBind_Pencil_Stroke)){
 				PencilStroke new_stroke;
 				new_stroke.size  = pencil_stroke_size;
 				new_stroke.color = pencil_stroke_color;
 				pencil_strokes.add(new_stroke);
 				pencil_stroke_start_pos = mouse_pos_world;
 			}
-			if(DeshInput->KeyDown(CanvasBind_Pencil_Stroke)){
+			if(key_down(CanvasBind_Pencil_Stroke)){
 				pencil_strokes[pencil_stroke_idx].pencil_points.add(mouse_pos_world);
 			}
-			if(DeshInput->KeyReleased(CanvasBind_Pencil_Stroke)){
+			if(key_released(CanvasBind_Pencil_Stroke)){
 				pencil_stroke_idx += 1;
 			}
-			if(DeshInput->KeyPressed(CanvasBind_Pencil_DeletePrevious)){ 
+			if(key_pressed(CanvasBind_Pencil_DeletePrevious)){ 
 				if(pencil_strokes.count){
 					pencil_strokes.pop();
 					if(pencil_stroke_idx) pencil_stroke_idx -= 1;
 				}
 			}
-			if     (DeshInput->KeyPressed(CanvasBind_Pencil_SizeIncrementBy1)){ pencil_stroke_size += 1; }
-			else if(DeshInput->KeyPressed(CanvasBind_Pencil_SizeIncrementBy5)){ pencil_stroke_size += 5; }
-			else if(DeshInput->KeyPressed(CanvasBind_Pencil_SizeDecrementBy1)){ pencil_stroke_size -= 1; }
-			else if(DeshInput->KeyPressed(CanvasBind_Pencil_SizeDecrementBy5)){ pencil_stroke_size -= 5; }
+			if     (DeshInput->scrollY > 0 && key_down(Key_LSHIFT)){ pencil_stroke_size += 1; }
+			else if(DeshInput->scrollY > 0 && key_down(Key_LCTRL)) { pencil_stroke_size += 5; }
+			else if(DeshInput->scrollY < 0 && key_down(Key_LSHIFT)){ pencil_stroke_size -= 1; }
+			else if(DeshInput->scrollY < 0 && key_down(Key_LCTRL)) { pencil_stroke_size -= 5; }
 			pencil_stroke_size = ((pencil_stroke_size < 1) ? 1 : ((pencil_stroke_size > 100) ? 100 : (pencil_stroke_size)));
-			if     (DeshInput->KeyPressed(CanvasBind_Pencil_DetailIncrementBy1)){ pencil_draw_skip_amount -= 1; }
-			else if(DeshInput->KeyPressed(CanvasBind_Pencil_DetailIncrementBy5)){ pencil_draw_skip_amount -= 5; }
-			else if(DeshInput->KeyPressed(CanvasBind_Pencil_DetailDecrementBy1)){ pencil_draw_skip_amount += 1; }
-			else if(DeshInput->KeyPressed(CanvasBind_Pencil_DetailDecrementBy5)){ pencil_draw_skip_amount += 5; }
+			if     (key_pressed(CanvasBind_Pencil_DetailIncrementBy1)){ pencil_draw_skip_amount -= 1; }
+			else if(key_pressed(CanvasBind_Pencil_DetailIncrementBy5)){ pencil_draw_skip_amount -= 5; }
+			else if(key_pressed(CanvasBind_Pencil_DetailDecrementBy1)){ pencil_draw_skip_amount += 1; }
+			else if(key_pressed(CanvasBind_Pencil_DetailDecrementBy5)){ pencil_draw_skip_amount += 5; }
 			pencil_draw_skip_amount = Clamp(pencil_draw_skip_amount, 1, 100);
 		}break;
 	}
